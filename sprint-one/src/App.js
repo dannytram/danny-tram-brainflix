392598ed0c475b585430b7a9985ab692184d6717
Danny Tram <tramdanny@hotmail.com>
2021-05-16 19:40:58 -0700

    Added desktop responsiveness

--- sprint-one/src/App.js
+++ sprint-one/src/App.js
@@ -1,84 +1,88 @@
-import React from 'react'
-import './styles/main.css'
-import Header from './components/Header'
-import HeroVideo from './components/HeroVideo'
-import HeroAbout from './components/HeroAbout'
-import CommentSection from './components/CommentSection'
-import CommentsPosted from './components/CommentsPosted'
-import VideoList from './components/VideoList'
-import videos from './data/video-details.json'
+import React from "react";
+import "./styles/main.css";
+import Header from "./components/Header";
+import HeroVideo from "./components/HeroVideo";
+import HeroAbout from "./components/HeroAbout";
+import CommentSection from "./components/CommentSection";
+import CommentsPosted from "./components/CommentsPosted";
+import VideoList from "./components/VideoList";
+import videos from "./data/video-details.json";
 
 class App extends React.Component {
   state = {
     videos,
     heroVideo: {
-      id: '1af0jruup5gu',
-      title: 'BMX Rampage: 2018 Highlights',
-      channel: 'Red Cow',
-      image: 'https://i.imgur.com/l2Xfgpl.jpg',
+      id: "1af0jruup5gu",
+      title: "BMX Rampage: 2018 Highlights",
+      channel: "Red Cow",
+      image: "https://i.imgur.com/l2Xfgpl.jpg",
       description:
-        'On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title',
-      views: '1,001,023',
-      likes: '110,985',
-      duration: '4:01',
-      video: 'https://project-2-api.herokuapp.com/stream',
+        "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title",
+      views: "1,001,023",
+      likes: "110,985",
+      duration: "4:01",
+      video: "https://project-2-api.herokuapp.com/stream",
       timestamp: 1545162149000,
       comments: [
         {
-          name: 'Micheal Lyons',
+          name: "Micheal Lyons",
           comment:
-            'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.',
-          id: '1ab6d9f6-da38-456e-9b09-ab0acd9ce818',
+            "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.",
+          id: "1ab6d9f6-da38-456e-9b09-ab0acd9ce818",
           likes: 0,
           timestamp: 1545162149000,
         },
         {
-          name: 'Gary Wong',
+          name: "Gary Wong",
           comment:
-            'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!',
-          id: 'cc6f173d-9e9d-4501-918d-bc11f15a8e14',
+            "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+          id: "cc6f173d-9e9d-4501-918d-bc11f15a8e14",
           likes: 0,
           timestamp: 1544595784046,
         },
         {
-          name: 'Theodore Duncan',
+          name: "Theodore Duncan",
           comment:
-            'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!',
-          id: '993f950f-df99-48e7-bd1e-d95003cc98f1',
+            "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+          id: "993f950f-df99-48e7-bd1e-d95003cc98f1",
           likes: 0,
           timestamp: 1542262984046,
         },
       ],
     },
-  }
+  };
 
   updatedVideoHandler = (id) => {
-    let updatedVideo = this.state.videos.find((video) => video.id === id)
+    let updatedVideo = this.state.videos.find((video) => video.id === id);
 
     this.setState({
       heroVideo: updatedVideo,
-    })
-  }
+    });
+  };
   render() {
     return (
       <div>
         <Header />
         <HeroVideo heroVideoThumbnail={this.state.heroVideo.image} />
-        <HeroAbout heroAboutDetails={this.state.heroVideo} />
-        <CommentSection
-          commentSectionComments={this.state.heroVideo.comments}
-        />
-        <CommentsPosted
-          commentSectionComments={this.state.heroVideo.comments}
-        />
-        <VideoList
-          videos={this.state.videos}
-          handleClick={this.updatedVideoHandler}
-          currentVideo={this.state.heroVideo.id}
-        />
+        <div className = "desktop-wrapper">
+          <div>
+            <HeroAbout heroAboutDetails={this.state.heroVideo} />
+            <CommentSection
+              commentSectionComments={this.state.heroVideo.comments}
+            />
+            <CommentsPosted
+              commentSectionComments={this.state.heroVideo.comments}
+            />
+          </div>
+          <VideoList
+            videos={this.state.videos}
+            handleClick={this.updatedVideoHandler}
+            currentVideo={this.state.heroVideo.id}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
