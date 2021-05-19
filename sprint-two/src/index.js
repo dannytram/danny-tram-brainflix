009b7ff882fdbfc394d29934504db9dca6e97e4d
Danny Tram <tramdanny@hotmail.com>
2021-05-18 21:15:00 -0700

    Added router

--- sprint-two/src/index.js
+++ sprint-two/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
