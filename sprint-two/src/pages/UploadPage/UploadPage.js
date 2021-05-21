89066b485b557cb9e194e3acf98a7b4c4ae68bf3
Danny Tram <tramdanny@hotmail.com>
2021-05-20 21:19:14 -0700

    Added styling to Upload Page for tablet and desktop

--- sprint-two/src/pages/UploadPage/UploadPage.js
+++ sprint-two/src/pages/UploadPage/UploadPage.js
@@ -7,28 +7,34 @@ class UploadPage extends React.Component {
         return (
             <div className="upload-page">
                 <h2 className="upload-page__title"> Upload Video</h2>
-                <div className="upload-page__thumbnail">
-                    <h3 className="upload-page__thumbnail-title">VIDEO THUMBNAIL</h3>
-                    <img 
-                    className="upload-page__thumbnail-img"
-                    src={preview}
-                    alt="Upload Your Video"
-                    />
+                <div className="upload-page__divider"></div>
+                <div className="upload-page__desktop-wrapper">
+                    <div className="upload-page__thumbnail">
+                        <h3 className="upload-page__thumbnail-title">VIDEO THUMBNAIL</h3>
+                        <img
+                            className="upload-page__thumbnail-img"
+                            src={preview}
+                            alt="Upload Your Video"
+                        />
+                    </div>
+                    <div className="upload-page__form">
+                        <h3 className="upload-page__video-title">TITLE YOUR VIDEO</h3>
+                        <textarea
+                            className="upload-page__video-title-form"
+                            placeholder="Add a title to your video"
+                        ></textarea>
+                        <h3 className="upload-page__video-description">ADD A VIDEO DESCRIPTION</h3>
+                        <textarea
+                            className="upload-page__video-description-form"
+                            placeholder="Add a description of your video"
+                        ></textarea>
+                    </div>
                 </div>
-                <div className="upload-page__form">
-                    <h3 className="upload-page__video-title">TITLE YOUR VIDEO</h3>
-                    <textarea 
-                    className="upload-page__video-title-form"
-                    placeholder="Add a title to your video"
-                    ></textarea>
-                    <h3 className="upload-page__video-description">ADD A VIDEO DESCRIPTION</h3>
-                    <textarea 
-                    className="upload-page__video-description-form"
-                    placeholder="Add a description of your video"
-                    ></textarea>
+                <div className="upload-page__divider"></div>
+                <div className="upload-page__btn-wrapper">
+                    <button className="upload-page__publish-btn">PUBLISH</button>
+                    <button className="upload-page__cancel-btn">CANCEL</button>
                 </div>
-                <button className = "upload-page__publish-btn">PUBLISH</button>
-                <button className = "upload-page__cancel-btn">CANCEL</button>
             </div>
         )
     }
