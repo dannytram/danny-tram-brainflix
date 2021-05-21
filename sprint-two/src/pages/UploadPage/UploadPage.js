be39daff09d76bf48e75cd90827881f40eb22c40
Danny Tram <tramdanny@hotmail.com>
2021-05-20 20:06:56 -0700

    Added styling for Upload Page mobile version

--- sprint-two/src/pages/UploadPage/UploadPage.js
+++ sprint-two/src/pages/UploadPage/UploadPage.js
@@ -1,23 +1,37 @@
 import React from 'react'
+import "../../styles/main.css";
+import preview from '../../assets/images/Upload-video-preview.jpg'
 
-function UploadPage() {
-    return (
-        <div className="upload-page">
-            <h2 className = "upload-page__title"> Upload Video</h2>
-            <div className = "upload-page__thumbnail">
-                <h3 className = "upload-page__thumbnail-title"></h3>
-                <img className = "upload-page__thumbnail-img"></img>
+class UploadPage extends React.Component {
+    render() {
+        return (
+            <div className="upload-page">
+                <h2 className="upload-page__title"> Upload Video</h2>
+                <div className="upload-page__thumbnail">
+                    <h3 className="upload-page__thumbnail-title">VIDEO THUMBNAIL</h3>
+                    <img 
+                    className="upload-page__thumbnail-img"
+                    src={preview}
+                    alt="Upload Your Video"
+                    />
+                </div>
+                <div className="upload-page__form">
+                    <h3 className="upload-page__video-title">TITLE YOUR VIDEO</h3>
+                    <textarea 
+                    className="upload-page__video-title-form"
+                    placeholder="Add a title to your video"
+                    ></textarea>
+                    <h3 className="upload-page__video-description">ADD A VIDEO DESCRIPTION</h3>
+                    <textarea 
+                    className="upload-page__video-description-form"
+                    placeholder="Add a description of your video"
+                    ></textarea>
+                </div>
+                <button className = "upload-page__publish-btn">PUBLISH</button>
+                <button className = "upload-page__cancel-btn">CANCEL</button>
             </div>
-            <div className = "upload-page__form">
-                <h3 className = "upload-page__video-title"></h3>
-                <textarea className = "upload-page__video-title-form"></textarea>
-                <h3 className = "upload-page__video-description"></h3>
-                <textarea className = "upload-page__video-description-form"></textarea>
-            </div>
-            <button>PUBLISH</button>
-            <button>CANCEL</button>
-        </div>
-    )
+        )
+    }
 }
 
 export default UploadPage
